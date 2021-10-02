714aad36596435cc2313a260cdf2c3303328d5aa
k_haru_love@i.softbank.jp <ec2-user@ip-172-31-11-126.ec2.internal>
2021-10-02 04:03:07 +0000

    fixed mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,16 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+    config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                  587,
+    domain:               'gmail.com',
+    user_name:            ENV['SEND_MAIL'],
+    password:             ENV['SEND_MAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto:  true
+  }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
