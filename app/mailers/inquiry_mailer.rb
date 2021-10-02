d11a5f46b579413675dc1f03c858178233da3c37
k_haru_love@i.softbank.jp <ec2-user@ip-172-31-11-126.ec2.internal>
2021-10-02 03:48:16 +0000

    fixed mailer

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -2,7 +2,7 @@ class InquiryMailer < ApplicationMailer
   
   def received_email(inquiry)
     @inquiry = inquiry
-    mail(:to => "190fe701@st.nufs.ac.jp", :subject => 'お問い合わせを承りました')
+    mail(:to => ENV['SEND_MAIL'], :subject => 'Webサイトからお問い合わせがありました。')
   end
   
 end
