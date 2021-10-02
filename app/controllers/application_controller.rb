d9c7a9e5d9d838f5107f13b644a7be56f45f5219
k_haru_love@i.softbank.jp <ec2-user@ip-172-31-11-126.ec2.internal>
2021-10-02 03:01:39 +0000

    fixed email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
     added_attrs = [:name, :email, :password, :password_confirmation, :remember_me]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
-    #devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
   
 end
